81ed61adc0454f9ad70cde2fa08a7cba88910a93
Jroddmqz <js.rdlf.mrqz.snts3@gmail.com>
2024-04-08 18:46:08 -0500

    update

--- ubot/__main__.py
+++ ubot/__main__.py
@@ -4,13 +4,11 @@ from .config import Config
 from .core import Core
 
 api = "https://api.telegram.org/bot" + Config.TokenBot
-#print(api)
 update_id = 0
 print("[+] Bot is actived !")
 print("[+] press control + c to exit !")
 res = requests.get(f"{api}/getme")
 bot_name = res.json()['result']['first_name']
-#print(res.json())
 print(f"[+] bot name : {bot_name}")
 print('~' * 40)
 while True:
@@ -22,7 +20,13 @@ while True:
 
         update = req["result"][0]
         print(update)
-        Core(update)
+        try:
+            x = update["message"]["chat"]["type"]
+            Core(update)
+        except:
+            continue
+
+
         update_id = update['update_id'] + 1
         print("~" * 40)
     except ConnectionError:
